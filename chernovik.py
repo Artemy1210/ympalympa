181ef26567758162cc66c59130f1260109edd111
Artemy1210 <99674655+Artemy1210@users.noreply.github.com>
2022-02-14 20:08:16 +0300

    Add files via upload

---
+++ chernovik.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+theta = np.linspace(0, np.pi, 3)
+print("theta = ", theta)
+print("sin(theta) = ", np.sin(theta))
+print("cos(theta) = ", np.cos(theta))
+print("tan(theta) = ", np.tan(theta))
+
+x = [1, 2, 3]
+print("x =", x)
+print("e^x =", np.exp(x))
+print("2^x =", np.exp2(x))
+print("3^x =", np.power(3, x))
+
+x = [1, 2, 4, 10]
+print("x =", x)
+print("ln(x) =", np.log(x))
+print("log2(x) =", np.log2(x))
+print("log10(x) =", np.log10(x))
